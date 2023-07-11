9658e1c58bcdcba49dd3bb84efd62d4f0cfdb6a0
Md. Golam Kausar <golam@mirinfosys.com>
2023-07-11 10:01:07 +0600

    change app name and icon

--- app/src/main/java/com/kausar/messmanagementapp/MainActivity.kt
+++ app/src/main/java/com/kausar/messmanagementapp/MainActivity.kt
@@ -6,15 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.kausar.messmanagementapp.navigation.SetupNavGraph
 import com.kausar.messmanagementapp.ui.theme.MessManagementAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/kausar/messmanagementapp/MessApplication.kt
@@ -0,0 +1,8 @@
+package com.kausar.messmanagementapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MessApplication: Application() {
+}
