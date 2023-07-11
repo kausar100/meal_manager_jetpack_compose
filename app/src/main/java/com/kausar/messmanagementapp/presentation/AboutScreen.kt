6976d6cbd11867070f3aff001d84fc0038debffc
Md. Golam Kausar <golam@mirinfosys.com>
2023-07-11 09:41:01 +0600

    update about screen

--- app/src/main/java/com/kausar/messmanagementapp/presentation/AboutScreen.kt
+++ app/src/main/java/com/kausar/messmanagementapp/presentation/AboutScreen.kt
@@ -1,18 +1,21 @@
-package com.kausar.messmanagementapp.ui.screens
+package com.kausar.messmanagementapp.presentation
 
 import android.content.Intent
-import androidx.compose.foundation.background
+import android.net.Uri
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,9 +35,9 @@ fun AboutScreen() {
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-
+        Spacer(modifier = Modifier.height(16.dp))
         Column {
             Text(
                 text = "This app helps with management which is useful for any mess member. The feature of this app are listed below : ",
@@ -56,7 +59,7 @@ fun AboutScreen() {
         Column(
             Modifier.padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
                 text = "Developed By",
@@ -65,6 +68,7 @@ fun AboutScreen() {
                 fontWeight = FontWeight.Bold,
                 color = Color.Black
             )
+            Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = "Md. Golam Kausar",
                 textAlign = TextAlign.Center,
@@ -73,33 +77,64 @@ fun AboutScreen() {
                 color = Color.Black
             )
             Text(
-                text = "Software Engineer\nContact No: 01315783246",
+                text = "Software Engineer",
                 textAlign = TextAlign.Center,
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black
             )
-            TextButton(
-                onClick = {
-                    val intent = Intent(Intent.ACTION_SEND).apply {
-                        type = "text/plain"
-                        putExtra(Intent.EXTRA_EMAIL, arrayOf("kausar.cse16@gmail.com"))
-                    }
-
-                    if (intent.resolveActivity(context.packageManager) != null) {
-                        context.startActivity(intent)
-                    }
-                }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "email",
+                )
+                Spacer(modifier = Modifier.width(4.dp))
                 Text(
-                    text = "Mail me: kausar.cse16@gmail.com",
-                    color = Color.Black
+                    text = "Mail Me",
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.clickable {
+                        val intent = Intent(Intent.ACTION_SEND).apply {
+                            type = "text/plain"
+                            putExtra(Intent.EXTRA_EMAIL, arrayOf("kausar.cse16@gmail.com"))
+                        }
+
+                        if (intent.resolveActivity(context.packageManager) != null) {
+                            context.startActivity(intent)
+                        }
+                    }
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Icon(imageVector = Icons.Default.Phone, contentDescription = "phone")
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "+8801315783246",
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.clickable {
+                        val intent = Intent(Intent.ACTION_DIAL).apply {
+                            data = Uri.parse("tel:"+"01315783246")
+                        }
+                        try {
+                            context.startActivity(intent)
+                        }catch (e: Exception){
+                            e.printStackTrace()
+                        }
+                    }
                 )
             }
 
         }
 
     }
+
 }
 
 @Preview(showBackground = true)
